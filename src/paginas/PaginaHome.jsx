f63d4942e43af21161b8fd8396eb8ee796552bf0
Fede Rascon <federascon@gmail.com>
2023-04-01 20:11:38 -0300

    version 1.0.2 de prueba

--- src/paginas/PaginaHome.jsx
+++ src/paginas/PaginaHome.jsx
@@ -1,27 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { LoginButton } from '../componentes/LoginButton'
 import { Header } from '../componentes/Header';
+import { useAccount } from 'wagmi'
+
+
 
 import './PaginaHome.css'
 import { SeccionUsuario } from '../componentes/SeccionUsuario';
 
 import { settings } from "./contrato/settings";
-import { useContractRead } from 'wagmi'
+import { useContract, useContractRead } from 'wagmi'
+
 
 // import { useContractReads } from 'wagmi'
 // import { useContract } from 'wagmi'
 
 const PaginaHome = () => {
+    const { address2, isConnecting, isDisconnected } = useAccount()
+
 
     const { addressContract, abi } = settings;
 
+    const contract = useContract({
+        address: addressContract,
+        abi
+    })
+
     const { balance, isError, isLoading } = useContractRead({
         address: addressContract,
-        abi: abi,
-        functionName: 'getBalance',
+        abi,
+        functionName: 'getAddress',
       })
   
     const [estaConectado, setEstaConectado] = useState(false);
+
+    console.log(address2)
 
     
     return (
@@ -35,7 +48,8 @@ const PaginaHome = () => {
                             />
                     {estaConectado && <SeccionUsuario />}
                     
-                <div>{isLoading ? <p>Loading...</p> : <p>BALANCE: {balance}</p>}</div> 
+                <div>{isLoading ? <p>Loading...</p> : <p>BALANCE: {balance} {contract.functions['getBalance']}</p>}</div> 
+                <div>{address2}</div>
                 </section>
             </body>
         </React.Fragment>
